39e9c2e3d6892ee910e2c5231a78bdda81f7e55e
Vinicius Carvalho <carvalho.viniciusluiz@gmail.com>
2022-04-07 15:52:20 -0300

    feat: first commit

---
+++ src/app.vars.ts
@@ -0,0 +1,16 @@
+import { ConfigService } from '@nestjs/config';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+const configService = new ConfigService();
+
+export const NODE_ENV = configService.get<string>('NODE_ENV');
+export const APP_PORT = configService.get<number>('APP_PORT');
+export const APP_VERSION = configService.get<string>('APP_VERSION');
+export const APP_VERSION_PREFIX = configService.get<string>('APP_VERSION_PREFIX');
+export const APP_BACKEND_API_URL = configService.get<string>('APP_BACKEND_API_URL');
+
+export const IS_PROD = NODE_ENV === 'production';
+export const IS_TEST = NODE_ENV === 'test';
+export const IS_DEV = !IS_TEST && !IS_PROD;
