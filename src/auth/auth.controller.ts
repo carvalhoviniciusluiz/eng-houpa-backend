5c181101308fb81c24ee62a3fad02026b37603dd
Vinicius Carvalho <carvalho.viniciusluiz@gmail.com>
2022-04-09 19:52:35 -0300

    chore: add documentation tag

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,9 @@
 import { BadRequestException, Body, Controller, Post } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 import { AuthService } from '~/auth/auth.service';
 import { AuthCredentialsRequestDTO, AuthSignUpRequestDTO } from '~/auth/dtos';
 
+@ApiTags('Auth')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
